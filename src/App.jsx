908ea7a4e705235e72e9397de40622cc23e59e7d
scof00 <scofprofessional@gmail.com>
2024-05-20 14:46:04 -0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,66 @@
+import { useEffect, useState } from "react";
+import { getAllTickets } from "./services/ticketService";
+import "./app.css";
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
-}
+  const [allTickets, setAllTickets] = useState([]);
+  const [showEmergencyOnly, setShowEmergencyOnly] = useState(false);
+  const [filteredTickets, setFilteredTickets] = useState([]);
+
+  useEffect(() => {
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray);
+      console.log("tickets set");
+    });
+  }, []);
+
+  useEffect(() => {
+    if (showEmergencyOnly === true) {
+      const emergencyTickets = allTickets.filter(
+        (ticket) => ticket.emergency === true
+      );
+      setFilteredTickets(emergencyTickets);
+    } else {
+      setFilteredTickets(allTickets);
+    }
+  }, [showEmergencyOnly, allTickets]);
+
+  return (
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <div>
+        <button
+          className="filter-btn btn-primary"
+          onClick={() => {
+            setShowEmergencyOnly(true);
+          }}
+        >
+          Emergency
+        </button>
+        <button
+          className="filter-btn btn-primary"
+          onClick={() => {
+            setShowEmergencyOnly(false);
+          }}
+        >
+          Show all
+        </button>
+      </div>
+      <article className="tickets"></article>
+      {filteredTickets.map((ticket) => {
+        return (
+          <section className="ticket" key={ticket.id}>
+            <header className="ticker-info">#{ticket.id}</header>
+            <div>{ticket.description}</div>
+            <footer>
+              <div>
+                <div className="ticket-info">Emergency</div>
+                <div>{ticket.emergency ? "yes" : "no"}</div>
+              </div>
+            </footer>
+          </section>
+        );
+      })}
+    </div>
+  );
+};
